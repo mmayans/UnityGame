8a8cada5266758a1f41ef1375d7443196e36e041
Madeline Mayans <82738387+mmayans@users.noreply.github.com>
2024-05-27 12:45:41 -0700

    map stuff

--- Unity Project/Assets/Map.cs
+++ Unity Project/Assets/Map.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
+    public List<Tile> tiles;
+
+    public List<Vector3> positions;
     // Start is called before the first frame update
     void Start()
     {
-        
+        positions.Add(  new Vector3 ( 0,0,0 ));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void Generate(){
+
     }
 }
